02a00bc476d97f2192c8902db06cba576f275c07
Francesco Dente <francesco.dente2003@gmail.com>
2024-06-09 17:12:59 +0200

    build: upload structure, create files

---
+++ mail/js/main.js
@@ -0,0 +1,6 @@
+const form = document.forms[0];
+const btnSend = document.getElementById("submit");
+
+let email;
+
+let whitelist = ["francesco.dente@gmail.com", "d17@gmail.com", "abomba@gmail.com", "cicciopasticcio35@gmail.com"];
