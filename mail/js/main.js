a837fe1206711f26419cb57416f1f45e64631688
Francesco Dente <francesco.dente2003@gmail.com>
2024-06-09 17:25:40 +0200

    minor: email complete

--- mail/js/main.js
+++ mail/js/main.js
@@ -1,6 +1,19 @@
 const form = document.forms[0];
 const btnSend = document.getElementById("submit");
 
-let email;
-
 let whitelist = ["francesco.dente@gmail.com", "d17@gmail.com", "abomba@gmail.com", "cicciopasticcio35@gmail.com"];
+
+let validated = false;
+
+while (!validated) {
+    let email = prompt("inserisci la tua email: ");
+
+    for (let i = 0; i < whitelist.length; i++) {
+        if (email == whitelist[i]) {
+            validated = true;
+            console.log("email accepted");
+        } else {
+            console.log("email not valid");
+        }
+    }
+}
